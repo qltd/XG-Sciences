8eeb1a8a69d951e0c5cc0abcf3dceaec46492b3b
Matt Restorff <inperpetuum@gmail.com>
2016-02-19 12:27:48 -0500

    removed duplicate google analytics

--- wp-content/themes/XGS/footer.php
+++ wp-content/themes/XGS/footer.php
@@ -3,7 +3,7 @@
   <div id="footer-wrap" class="clearfix">
     <footer>
       <ul>
-        <?php wp_nav_menu( array('theme_location' => 'footer', 'container' => '', 'items_wrap' => '%3$s' )); ?> 
+        <?php wp_nav_menu( array('theme_location' => 'footer', 'container' => '', 'items_wrap' => '%3$s' )); ?>
         <li><a href="http://www.linkedin.com/company/xg-sciences" target="_blank"><i class="icon-linkedin-sign"></i></a></li>
       </ul><br />
       <p>&copy; <?php echo date('Y'); ?> XG Sciences <span>&middot;</span>  3101 Grand Oak Drive - Lansing, MI 48911 <span>&middot;</span> <a href="http://www.hiledesign.com" target="_blank">Website Design by Hile Design</a></p>
@@ -17,14 +17,6 @@
 	<script src="<?php bloginfo('template_directory'); ?>/js/libs/bootstrap.min.js"></script>
   <script src="<?php bloginfo('template_directory'); ?>/js/plugins.js"></script>
 	<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
-
-  <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-  </script>
-
 
 </body>
 </html>
